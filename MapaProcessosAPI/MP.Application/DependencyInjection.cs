ad9659f493083f60ff21c33c58c79f461b7b6b8b
Vinícius Resende de Oliveira <vinicius_rr1@hotmail.com>
2024-04-14 23:41:37 -0300

    Servico de mapeamento de objeto e validação dos dados

---
+++ MapaProcessosAPI/MP.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MP.Application.Interfaces;
+using MP.Application.OpenApp;
+using System.Reflection;
+
+namespace MP.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddTransient<IProcessoOpenApp, ProcessoOpenApp>();
+
+            return services;
+        }
+    }
+}
---
+++ MapaProcessosAPI/MP.Application/OpenApp/ProcessoOpenApp.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using MP.Application.DTOs.Request;
+using MP.Application.DTOs.Result;
+using MP.Application.Interfaces;
+using MP.Domain.Entities;
+using MP.Domain.Interfaces.Services;
+
+namespace MP.Application.OpenApp
+{
+    public class ProcessoOpenApp : IProcessoOpenApp
+    {
+        private readonly IProcessosService _processosService;
+        private readonly IMapper _mapper;
+
+        public ProcessoOpenApp(IProcessosService processosService, IMapper mapper)
+        {
+            _processosService = processosService;
+            _mapper = mapper;
+        }
+
+        public async Task<AddProcessoResult> AddAsync(AddProcessoRequest request)
+        {
+            request.Validate();
+
+            var result = await _processosService.AddAsync(_mapper.Map<Processo>(request));
+
+            return _mapper.Map<AddProcessoResult>(result);
+        }
+
+        public async Task<List<GetProcessoResult>> GetAllAsync()
+        {
+            var result = await _processosService.GetAllAsync();
+
+            return _mapper.Map<List<GetProcessoResult>>(result);
+        }
+
+        public async Task<GetByIdProcesso> GetByIdAsync(GetByIdProcessoRequest request)
+        {
+            request.Validate();
+
+            var result = await _processosService.GetbyId(request.Id);
+
+            return _mapper.Map<GetByIdProcesso>(result);
+        }
+
+        public async Task<List<GetProcessoResult>> GetSubProcessosAsync(GetByIdProcessoRequest request)
+        {
+            request.Validate();
+
+            var result = await _processosService.GetSubProcesso(request.Id);
+
+            return _mapper.Map<List<GetProcessoResult>>(result);
+        }
+
+        public async Task RemoveAsync(RemoveProcessoRequest request)
+        {
+            request.Validate();
+
+            await _processosService.Delete(request.Id);
+        }
+
+        public async Task<UpdateProcessoResult> UpdateAsync(UpdateProcessoRequest request)
+        {
+            request.Validate();
+
+            var result = await _processosService.Update(_mapper.Map<Processo>(request));
+
+            return _mapper.Map<UpdateProcessoResult>(result);
+        }
+
+        public async Task<UpdateProcessoResult> UpdateProcessoPaiAsync(UpdateProcessoPaiRequest request)
+        {
+            request.Validate();
+
+            var result = await _processosService.UpdateProcessoPai(request.Id, request.IdProcessoPai);
+
+            return _mapper.Map<UpdateProcessoResult>(result);
+        }
+    }
+}
